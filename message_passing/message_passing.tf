8ee73c59466ce4fff053c62327bec8e1eca6d204
Birbwell <jh375195@dal.ca>
2025-07-20 20:30:36 -0300

    added barebones functionality to the message passing module (the lambda prints "hello, lambda!") the sns/sqs topics have been created, its just a matter of linking them to the lambda function

--- message_passing/message_passing.tf
+++ message_passing/message_passing.tf
@@ -1,11 +1,11 @@
 
 data "archive_file" "forward_message_lambda" {
-  type = "zip"
+  type        = "zip"
   source_file = "./message_passing/forward_message_lambda.py"
   output_path = "./lambda_zip_archives/forward_message_lambda.zip"
 }
 
-data "aws_iam_policy_document" "sns_lambda_assume_role" {
+data "aws_iam_policy_document" "feedback_lambda_assume_role" {
   statement {
     effect = "Allow"
 
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "sns_lambda_assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "sns_lambda_policies" {
+data "aws_iam_policy_document" "feedback_lambda_policies" {
   statement {
     effect = "Allow"
     actions = [
@@ -34,13 +34,34 @@ data "aws_iam_policy_document" "sns_lambda_policies" {
       "dynamodb:PutItem",
       "dynamodb:GetItem"
     ]
-    resources = []
+    resources = [aws_dynamodb_table.communication_log_table.arn]
   }
 }
 
+resource "aws_iam_role" "feedback_lambda_role" {
+  name               = "SnsLambdaRole"
+  assume_role_policy = data.aws_iam_policy_document.feedback_lambda_assume_role.json
+}
+
+resource "aws_iam_policy" "feedback_policy" {
+  name   = "FeedbackLambdaPolicy"
+  policy = data.aws_iam_policy_document.feedback_lambda_policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "sns_lambda_policy_attachment" {
+  role       = aws_iam_role.feedback_lambda_role.name
+  policy_arn = aws_iam_policy.feedback_policy.arn
+}
+
 resource "aws_dynamodb_table" "communication_log_table" {
-  name = "CommunicationLogTable"
+  name         = "CommunicationLogTable"
   billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "N"
+  }
 }
 
 resource "aws_sns_topic" "feedback_sns" {
@@ -49,4 +70,13 @@ resource "aws_sns_topic" "feedback_sns" {
 
 resource "aws_sqs_queue" "feedback_sqs" {
   name = "FeedbackSQS"
+}
+
+resource "aws_lambda_function" "feedback_lambda" {
+  function_name    = "FeedbackLambda"
+  role             = aws_iam_role.feedback_lambda_role.arn
+  handler          = "forward_message_lambda.lambda_handler"
+  filename         = data.archive_file.forward_message_lambda.output_path
+  source_code_hash = data.archive_file.forward_message_lambda.output_base64sha256
+  runtime          = "python3.11"
 }
